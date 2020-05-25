9f6d80b6b58b64cf0b9db14764bd73ed7f717c0d
JulianCode <hijuliansosa@protonmail.com>
2020-05-25 14:43:36 -0500

    es10: flatMap

--- js/02-flatMap.js
+++ js/02-flatMap.js
@@ -3,3 +3,9 @@ const productos = [
     { nombre: 'Producto 2', precio: 30},
     { nombre: 'Producto 3', precio: 40}
 ];
+let result;
+result = productos.flatMap((producto) => [producto.nombre])
+result = productos.flatMap((producto) => [producto.precio])
+result = productos.flatMap((producto) => [producto.nombre, producto.precio])
+
+console.log(result);
